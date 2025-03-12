1573c21cea72174a5ca8c3b242a10dca4f71d672
empeje <11813607+empeje@users.noreply.github.com>
2025-03-12 21:47:34 +0100

    Initial commit

---
+++ src/main/java/io/mpj/webpigram/WebpigramApplication.java
@@ -0,0 +1,13 @@
+package io.mpj.webpigram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpigramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpigramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mpj/webpigram/WebpigramApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mpj.webpigram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpigramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
